50889f790ed29b818b0596394c6050b83ee3a497
like <20783506632@qq.com>
2017-06-24 11:03:47 +0800

    implement change jobs states

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,6 +6,10 @@ class JobsController < ApplicationController
 
     def show
         @job = Job.find(params[:id])
+        if @job.is_hidden
+            flash[:waring] = "这个工作没发布"
+            redirect_to root_path
+        end
     end
 
     def edit
